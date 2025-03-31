9bf99480a597cabd6505f36916bb7965fdb07bb2
sarramedd <sarramadeh@gmail.com>
2025-03-31 23:47:29 +0100

    2nd

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['../../../assets/css/style.css',
+              '../../../assets/css/bootstrap.min.css',
+            '../../../assets/css/nouislider.min.css',
+          '../../../assets/css/nouislider.min.css']
 })
 export class ProductComponent {
 
